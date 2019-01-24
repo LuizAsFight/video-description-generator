da205470a42e4889ff09a234bf86f086e8ff0459
Luiz Felipe Bolsoni Gomes <felipebolsonigomes@gmail.com>
2019-01-24 02:24:04 -0200

    chore

--- src/index.js
+++ src/index.js
@@ -4,16 +4,26 @@ const VideoDescriptionGenerator = {
   clockIcon: '⏰',
   afterTitleDivider: '       ',
   afterLinkDivider: '         ',
-  generateTextToCopy: (items) => {
+  generateDescription: (items) => {
     if (!Array.isArray(items)) {
       items = [items];
     }
 
     return items.reduce((prev, { title, link, time }, i) => {
-      prev += `${title ? `${VideoDescriptionGenerator.titleIcon} ${title}${VideoDescriptionGenerator.afterTitleDivider}` : ''}${link ? `${VideoDescriptionGenerator.linkIcon} ${link}${VideoDescriptionGenerator.afterLinkDivider}` : ''}${`${VideoDescriptionGenerator.clockIcon} ${time}`}
+      console.log({ title, link, time });
+      const _time = (typeof time === 'string' && time.indexOf(':')) ? time : VideoDescriptionGenerator._parseTime(time);
+      prev += `${title ? `${VideoDescriptionGenerator.titleIcon} ${title}${VideoDescriptionGenerator.afterTitleDivider}` : ''}${link ? `${VideoDescriptionGenerator.linkIcon} ${link}${VideoDescriptionGenerator.afterLinkDivider}` : ''}${`${VideoDescriptionGenerator.clockIcon} ${_time}`}
 `;
       return prev;
     }, ``);
+  },
+  _parseTime: (_time) => {
+    const time = parseInt(_time, 10);
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+    const strSeconds = `${seconds.toString().length === 1 ? '0' : ''}${seconds.toString()}`;
+
+    return `${minutes}:${strSeconds}`;
   }
 }
 
